bd6e1c3404992a985125d964ee9add57b128f0c9
Edward Diaz <ead0601@gmail.com>
2022-08-23 05:24:11 -0400

    added orange block which is a double and into a nor

---
+++ mioc-and2-nor/src/mioc_and2_nor_nmos.v
@@ -0,0 +1,45 @@
+// mioc-and2-nor
+//
+// Edward Diaz , 08_21_22
+//
+// This is a mos representation of the original mos layout
+//  configuration that uses an open drain configuration.
+
+// mioc-and2-nor
+//
+module mioc_and2_nor_nmos(
+    z,
+
+    in1,            
+    in2,            
+    in3,            
+    in4             
+    );
+
+   output z;
+
+   input  in1;
+   input  in2;
+   input  in3;
+   input  in4;
+
+   supply0 GND;
+
+   wire     m1d, m2d, m4d;   
+   
+   //nmos (drain,source,gate)
+
+   // LEVEL 1
+   pullup p1 (m2d);
+   
+   nmos   m1 (m1d,GND,in1);
+
+   nmos   m2 (m2d,m1d,in2);
+
+   nmos   m3 (m2d,m4d,in3);
+
+   nmos   m4 (m4d,GND,in4);
+   
+   assign z = m2d;
+
+endmodule
---
+++ mioc-and2-nor/src/testbench.v
@@ -0,0 +1,78 @@
+`timescale 1ns / 1 ns
+
+`define NULL 0    
+
+// Generic testbench to drive inputs from a file
+//
+// MIOC ASIC testing of mos gate - Edward Diaz 
+//
+module testbench ();
+   
+   wire z;
+ 
+   reg in1,in2,in3,in4;
+   reg [3:0] input_data;
+
+   integer   i,count;
+   integer   file_r, file_w;
+
+   // Init file handles and waveform dumping
+   //
+   initial begin
+      // Dump waves
+      $dumpfile("waves.vcd");
+      $dumpvars(0, testbench);
+
+      // Open file handle
+      file_r = $fopen("./DATA_IN/pat.txt", "r");
+      file_w = $fopen("./DATA_OUT/pat.txt", "w");
+      
+      if (file_r == `NULL) begin
+	 $display("Error: File handle was NULL");
+	 $finish;
+      end
+
+      // Write header
+      $fwrite(file_w, "in1 in2 in3 in4 - z\n");
+
+      // STDOUT as well
+      $display("in1 in2 in3 in4 - z\n");      
+
+      // Give a reset value
+      //
+      in1 <= 0;
+      in2 <= 0;
+      in3 <= 0;
+      in4 <= 0;  
+
+      // Drive patterns
+      //
+      while (1) begin
+	 count = $fscanf(file_r, "%b\n", input_data);
+	 #100;
+	 if (!$feof(file_r)) begin
+	    in1 = input_data[3];
+	    in2 = input_data[2];	    
+	    in3 = input_data[1];
+	    in4 = input_data[0];
+	    $fwrite(file_w, "%b %b %b %b - %b\n",in1,in2,in3,in4,z);
+    	    $display("%b %b %b %b - %b",in1,in2,in3,in4,z); 
+	 end
+	 else begin
+	    $fclose(file_r);
+	    $fclose(file_w);	    
+	    $finish;
+	 end
+      end
+   end
+   
+   // Design
+   //
+   mioc_and2_nor_nmos reg1(.z(z),
+		       .in1(in1),
+		       .in2(in2),
+		       .in3(in3),
+		       .in4(in4)
+		       );
+
+endmodule
